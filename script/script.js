b736466155efb993b471b06722d4826e32c9fa3c
Martynas <martynas@marcinonis.lt>
2017-11-29 16:13:50 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,58 @@
+$( document ).ready(function() {
+    
+// hide-show answers
+
+$('#send-o').click(function(){
+    $('.wu-content').addClass('hidden');
+    $('#send-o-a').removeClass("hidden");
+
+});
+
+$('#send-p').click(function(){
+    $('.wu-content').addClass('hidden');
+    $('#send-p-a').removeClass("hidden");
+
+});
+
+$('#receive').click(function(){
+    $('.wu-content').addClass('hidden');
+    $('#receive-a').removeClass("hidden");
+
+});
+
+$('#reg').click(function(){
+    $('.wu-content').addClass('hidden');
+    $('#reg-a').removeClass("hidden");
+
+});
+
+$('#ident').click(function(){
+    $('.wu-content').addClass('hidden');
+    $('#ident-a').removeClass("hidden");
+
+});
+
+$('#protec').click(function(){
+    $('.wu-content').addClass('hidden');
+    $('#protec-a').removeClass("hidden");
+
+});
+
+$('#loc').click(function(){
+    $('.wu-content').addClass('hidden');
+    $('#loc-a').removeClass("hidden");
+
+});
+
+$('#loyal').click(function(){
+    $('.wu-content').addClass('hidden');
+    $('#loyal-a').removeClass("hidden");
+
+});
+
+// close button
+
+$('.close-btn').click(function(){
+    $('.wu-content').addClass('hidden');
+});
+});
